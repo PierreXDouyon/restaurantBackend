148f4a44667070132dab3c2165e8f87b5a16917f
Pierre Douyon <pierredouyon514@gmail.com>
2023-09-05 22:21:33 -0400

    Updated Routes for Seating Info

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post('/all',  reservationController.getAll);
 router.post('/create',  reservationController.Create);
+router.post('/check',  reservationController.CheckResevation);
 router.delete('/delete',  reservationController.Delete);
 
 module.exports = router;
