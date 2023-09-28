60ab6623863ec6a57663e538178954fe7f8d1c1b
Pierre Douyon <pierredouyon514@gmail.com>
2023-09-28 10:01:17 -0400

    Fix the cors error issue on Codespace run

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const app = express();
 const port = 3000;
 const routes = require("./routes/index.js");
 var cors = require("cors");
-app.use(cors({
-  origin: 'http://localhost:8080',
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true,
-}));
+app.use(cors());
 const bodyParser = require("body-parser");
 // parse application/json
 app.use(bodyParser.json());
